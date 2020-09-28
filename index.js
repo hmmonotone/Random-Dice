b544fe95e3b5835c543b65654017eb813a575e4b
Himanshu Mishra <68955197+hmmonotone@users.noreply.github.com>
2020-09-29 02:00:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var randomNumber1=Math.random();
-randomNumber1=Math.floor((randomNumber1)*6)+1;
-var randomNumber2=Math.random();
-randomNumber2=Math.floor((randomNumber2)*6)+1;
-var rand_img1="images/"+"dice"+randomNumber1+".png";
-var rand_img2="images/"+"dice"+randomNumber2+".png";
-document.querySelector(".img1").setAttribute("src",rand_img1);
-document.querySelector(".img2").setAttribute("src",rand_img2);
-if(randomNumber1>randomNumber2)
-{
-  document.querySelector("h1").innerHTML="🥇Player 1 Wins!";
 
-}
-else if (randomNumber1<randomNumber2) {
-  document.querySelector("h1").innerHTML="Player 2 Wins🥇";
-}
-else{
-  document.querySelector("h1").innerHTML="Draw!";
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
